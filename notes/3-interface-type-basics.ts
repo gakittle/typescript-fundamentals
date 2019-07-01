e0ec43f9dedbda51d52cff47497e6971391f04b0
Greg Kittleson <greg.kittleson@gmail.com>
2019-07-01 12:19:51 -0700

    Implement part 3, interfaces & types

--- notes/3-interface-type-basics.ts
+++ notes/3-interface-type-basics.ts
@@ -1,52 +1,55 @@
-import { HasPhoneNumber, HasEmail } from "./1-basics";
+import { HasPhoneNumber, HasEmail } from './1-basics';
 
 //== TYPE ALIAS ==//
 /**
  * (1) Type aliases allow us to give a type a name
  */
-// type StringOrNumber = string | number;
+type StringOrNumber = string | number;
 
-// // this is the ONLY time you'll see a type on the RHS of assignment
-// type HasName = { name: string };
+// this is the ONLY time you'll see a type on the RHS of assignment
+type HasName = { name: string };
 
 // // 🚨 self-referencing types don't work! (we'll get there!)
-// type NumVal = 1 | 2 | 3 | NumArr;
-// type NumArr = NumVal[];
+type NumVal = 1 | 2 | 3 | NumArr;
+// this breaks from recursive look-ups: type NumArr = NumVal[];
+// this works:
+interface NumArr extends Array<NumVal> {}
+const x: NumVal = [1, 1, 1, 1, 1, [1, 2, 3, 1]];
 
 // == INTERFACE == //
 /**
  * (2) Interfaces can extend from other interfaces
  */
 
-// export interface HasInternationalPhoneNumber extends HasPhoneNumber {
-//   countryCode: string;
-// }
+export interface HasInternationalPhoneNumber extends HasPhoneNumber {
+  countryCode: string;
+}
 
 /**
  * (3) they can also be used to describe call signatures
  */
 
-// interface ContactMessenger1 {
-//   (contact: HasEmail | HasPhoneNumber, message: string): void;
-// }
+interface ContactMessenger1 {
+  (contact: HasEmail | HasPhoneNumber, message: string): void;
+}
 
-// type ContactMessenger2 = (
-//   contact: HasEmail | HasPhoneNumber,
-//   message: string
-// ) => void;
+type ContactMessenger2 = (
+  contact: HasEmail | HasPhoneNumber,
+  message: string
+) => void;
 
-// // NOTE: we don't need type annotations for contact or message
-// const emailer: ContactMessenger1 = (_ contact, _message) => {
-//   /** ... */
-// };
+// NOTE: we don't need type annotations for contact or message
+const emailer: ContactMessenger1 = (_contact, _message) => {
+  /** ... */
+};
 
 /**
  * (4) construct signatures can be described as well
  */
 
-// interface ContactConstructor {
-//   new (...args: any[]): HasEmail | HasPhoneNumber;
-// }
+interface ContactConstructor {
+  new (...args: any[]): HasEmail | HasPhoneNumber;
+}
 
 /**
  * (5) index signatures describe how a type will respond to property access
@@ -60,20 +63,31 @@ import { HasPhoneNumber, HasEmail } from "./1-basics";
  * }
  */
 
-// interface PhoneNumberDict {
-//   // arr[0],  foo['myProp']
-//   [numberName: string]:
-//     | undefined
-//     | {
-//         areaCode: number;
-//         num: number;
-//       };
-// }
+interface PhoneNumberDict {
+  // arr[0],  foo['myProp']
+  [numberName: string]:
+    | undefined
+    | {
+        areaCode: number;
+        num: number;
+      };
+}
 
-// const phoneDict: PhoneNumberDict = {
-//   office: { areaCode: 321, num: 5551212 },
-//   home: { areaCode: 321, num: 5550010 } // try editing me
-// };
+const d: PhoneNumberDict = {};
+// conditions can be used to "narrow" types
+if (d.abc) {
+  console.log(d.abc);
+}
+
+if (typeof d.abc === 'string') {
+  d.abc;
+  console.log('type never, as in this is never gonna happen');
+}
+
+const phoneDict: PhoneNumberDict = {
+  office: { areaCode: 321, num: 5551212 },
+  home: { areaCode: 321, num: 5550010 } // try editing me
+};
 
 // at most, a type may have one string and one number index signature
 
@@ -81,26 +95,26 @@ import { HasPhoneNumber, HasEmail } from "./1-basics";
  * (6) they may be used in combination with other types
  */
 
-// // augment the existing PhoneNumberDict
-// // i.e., imported it from a library, adding stuff to it
-// interface PhoneNumberDict {
-//   home: {
-//     /**
-//      * (7) interfaces are "open", meaning any declarations of the
-//      * -   same name are merged
-//      */
-//     areaCode: number;
-//     num: number;
-//   };
-//   office: {
-//     areaCode: number;
-//     num: number;
-//   };
-// }
+// augment the existing PhoneNumberDict
+// i.e., imported it from a library, adding stuff to it
+interface PhoneNumberDict {
+  home: {
+    /**
+     * (7) interfaces are "open", meaning any declarations of the
+     * -   same name are merged
+     */
+    areaCode: number;
+    num: number;
+  };
+  office: {
+    areaCode: number;
+    num: number;
+  };
+}
 
-// phoneDict.home;   // definitely present
-// phoneDict.office; // definitely present
-// phoneDict.mobile; // MAYBE present
+phoneDict.home; // definitely present
+phoneDict.office; // definitely present
+phoneDict.mobile; // MAYBE present
 
 // == TYPE ALIASES vs INTERFACES == //
 
